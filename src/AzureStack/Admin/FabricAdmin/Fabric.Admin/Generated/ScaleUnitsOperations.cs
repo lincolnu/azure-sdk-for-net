500bb3449793d98be2c0ae122c1c31799a569e98
Lincoln Uyeda <lincolnu@microsoft.com>
2018-07-10 13:40:27 -0700

    Update scale unit operations

--- src/AzureStack/Admin/FabricAdmin/Fabric.Admin/Generated/ScaleUnitsOperations.cs
+++ src/AzureStack/Admin/FabricAdmin/Fabric.Admin/Generated/ScaleUnitsOperations.cs
@@ -855,7 +855,7 @@ namespace Microsoft.AzureStack.Management.Fabric.Admin
             HttpStatusCode _statusCode = _httpResponse.StatusCode;
             cancellationToken.ThrowIfCancellationRequested();
             string _responseContent = null;
-            if ((int)_statusCode != 200 && (int)_statusCode != 202 && (int)_statusCode != 500)
+            if ((int)_statusCode != 200 && (int)_statusCode != 202)
             {
                 var ex = new CloudException(string.Format("Operation returned an invalid status code '{0}'", _statusCode));
                 try
